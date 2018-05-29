352e185814be92e83c2aab3abc1bf716687a0f5b
Knight-boy <854437169@qq.com>
2018-05-29 20:58:00 +0800

    程序入口

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');      //Third-party middleware
-var cookieParser = require('cookie-parser');//Third-party middleware
-var bodyParser = require('body-parser');    //Third-party middleware
-// var settings = require('./settings');
-// var session = require('express-session');
-// var flash = require('connect-flash');
-
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var flash = require("connect-flash");
+var session = require('express-session');
 var app = express();
 
 // view engine setup
@@ -15,49 +14,32 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));  //false:用qs库解析Url编码；true:用字符串库解析
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(flash());
-// app.use(session({
-    // secret:settings.cookieSecret,
-    // store: new MongoStore({
-    //     db: settings.db
-    // })
-// }));
 
-app.use('/', require('./routes/index'));//index
-app.use('/users', require('./routes/users'));//users
+app.use(session({
+    secret:'keyboard cat',
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: true,
+}));
+app.use(flash());
 
-app.post('/post',require('./routes/posts'));
-app.use('/reg',require('./routes/reg'));
-app.post('/reg',require('./routes/doReg'));
-app.use('/login',require('./routes/login'));
-app.post('/login',require('./routes/doLogin'));
-app.use('/logout',require('./routes/logout'));
+app.use(function(req,res,next){
+  res.locals.session = req.session;
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+});
 
-
-// app.dynamicHelpers({
-//     user:function(req,res) {
-//         return req.session.user;
-//     },
-//     error: function(req,res) {
-//         var err = req.flash('error');
-//         if(err.length)
-//             return err;
-//         else
-//             return null;
-//     },
-//     success: function(req,res) {
-//         var succ = req.flash('success');
-//         if(succ.length)
-//             return succ;
-//         else
-//             return null;
-//     }
-// });
+app.use('/', require('./routes/index'));
+app.use('/', require('./routes/users'));
+app.use('/',require('./routes/reg'));
+app.use('/',require('./routes/login'));
+app.use('/',require('./routes/content'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -75,12 +57,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
-
-var port = process.env.PORT || 8800;
-
-app.listen(port,() => {
-    console.log(`Magic starts on port : ${port}`);
 });
 
 module.exports = app;
